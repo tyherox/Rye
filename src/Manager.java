545e617e46547e60fa2df6eeccd565374c2b24cd
tyherox <silverbae@gmail.com>
2015-03-02 11:37:16 +0900

    windows update
    update

--- src/Manager.java
+++ src/Manager.java
@@ -1,12 +1,29 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * Created by 의현 on 2015-02-24.
  */
 public class Manager extends JPanel {
 
+    boolean extended = false;
+
     public Manager(){
         setBackground(Color.DARK_GRAY);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if(extended==false) {
+                    setBounds(getX()-350,getY(),getWidth()+350,getHeight());
+                    extended=true;
+                }
+                else if(extended==true) {
+                    setBounds(getX()+350,getY(),getWidth()-350,getHeight());
+                    extended=false;
+                }
+            }
+        });
     }
 }
